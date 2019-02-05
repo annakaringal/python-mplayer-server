ac33edb319eb74814999ea9da3ec06154a41e564
Anna Karingal <akaringal@vistarmedia.com>
2019-02-05 14:36:53 -0500

    Add mplayer assets.

--- run.py
+++ run.py
@@ -7,36 +7,40 @@ import mplayer
 
 HOST = 'localhost'
 PORT = 9998
+FILE_PATH = './assets/test8.mkv'
 
 class MPlayerServer(BaseHTTPRequestHandler):
-
-    def init(self):
-        self.player = mplayer.Player()
-
-    def _set_headers(self):
-        self.send_response(200)
+    def _set_headers(self, code=200):
+        self.send_response(code)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
 
     def _send_response(self, response):
         self.wfile.write((json.dumps(response)).encode())
 
-    def do_GET(self):
-        self._set_headers()
-        self._send_response({'status': 'ok'})
+    def _create_player(self):
+        player = mplayer.Player()
+        player.fullscreen = True
+        return player
 
     def do_HEAD(self):
         self._set_headers()
 
     def do_POST(self):
-        self._set_headers()
+        if self.path != '/play':
+            self._set_headers(404)
+            return
+
+        player = self._create_player()
+        player.loadfile(FILE_PATH)
+
         self._send_response({'status': 'ok'})
 
 
 def run():
     server_address = (HOST, PORT)
     httpd = HTTPServer(server_address, MPlayerServer)
-    print('Starting Mplayer server...i')
+    print('Starting Mplayer server...')
     httpd.serve_forever()
 
 if __name__ == "__main__":
